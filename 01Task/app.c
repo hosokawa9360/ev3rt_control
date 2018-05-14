bc795061bbd36b49f5ee60f3c2009f35ed4cbba5
Junya Hosokawa <hosokawa@morijyobi.ac.jp>
2018-05-14 12:17:16 +0900

    tslp_task(100)

--- 01Task/app.c
+++ 01Task/app.c
@@ -7,19 +7,25 @@
 void task1(intptr_t unused) {
 	char m[32];
 	static int count = 0;
+	while(1) {
 	sprintf(m , "Task1:%3d" ,count++);
 	ev3_lcd_set_font(EV3_FONT_MEDIUM);
-   ev3_lcd_draw_string(m, 20 ,20);
-   ext_tsk();
+    ev3_lcd_draw_string(m, 20 ,20);
+	tslp_tsk(100);
+	}
+   //ext_tsk();
 }
 
 void task2(intptr_t unused) {
 	char m[32];
 	static int count = 0;
+	while(1) {
 	sprintf(m , "Task2:%3d" ,count++);
 	ev3_lcd_set_font(EV3_FONT_MEDIUM);
    ev3_lcd_draw_string(m, 20 ,40);
-   ext_tsk();
+	tslp_tsk(100);
+	}
+   //ext_tsk();
 }
 
 
