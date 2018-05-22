38ff7b711e12bf0571f48147ca795f6722157a77
Junya Hosokawa <hosokawa@morijyobi.ac.jp>
2018-05-22 11:49:10 +0900

    02Task　complete

---
+++ 02Task/app.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ev3api.h"
+
+#ifndef TOPPERS_MACRO_ONLY
+
+extern void	task_cyc1(intptr_t exinf);
+
+#endif /* TOPPERS_MACRO_ONLY */
+
+#ifdef __cplusplus
+}
+#endif	
---
+++ bluetooth_tlansfer_ev3rt.sh
@@ -0,0 +1,3 @@
+#ttpmacro="/cygdrive/c/Program Files (x86)/teraterm/ttpmacro.exe" 
+#cygstart "$ttpmacro"  ./"zmodem.ttl" #←$ttpmacroを""で囲むのを忘れるとエラーが出る
+cygstart ./zmodem.ttl
